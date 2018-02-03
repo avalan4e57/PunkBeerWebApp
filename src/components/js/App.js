93701b806e1245e350b9d766aed96bbca1399211
Igor Chernega <am.igor.chernega@gmail.com>
2018-02-03 21:13:28 +0200

    First scratch of the hole project

--- src/components/js/App.js
+++ src/components/js/App.js
@@ -14,45 +14,8 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      cards: [
-        // {
-        //   id: 1,
-        //   name: 'Buzz1',
-        //   image: 'https://images.punkapi.com/v2/2.png',
-        //   description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio voluptates tenetur odit fugit, quos laboriosam facilis velit eos commodi omnis pariatur nesciunt ipsa perspiciatis vitae doloremque odio! Error, asperiores unde!',
-        //   selected: false
-        // },
-        // {
-        //   id: 2,
-        //   name: 'Buzz2',
-        //   image: 'https://images.punkapi.com/v2/2.png',
-        //   description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio voluptates tenetur odit fugit, quos laboriosam facilis velit eos commodi omnis pariatur nesciunt ipsa perspiciatis vitae doloremque odio! Error, asperiores unde!',
-        //   selected: false
-        // },
-        // {
-        //   id: 3,
-        //   name: 'Buzz3',
-        //   image: 'https://images.punkapi.com/v2/2.png',
-        //   description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio voluptates tenetur odit fugit, quos laboriosam facilis velit eos commodi omnis pariatur nesciunt ipsa perspiciatis vitae doloremque odio! Error, asperiores unde!',
-        //   selected: false
-        // },
-        // {
-        //   id: 4,
-        //   name: 'Buzz4',
-        //   image: 'https://images.punkapi.com/v2/2.png',
-        //   description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio voluptates tenetur odit fugit, quos laboriosam facilis velit eos commodi omnis pariatur nesciunt ipsa perspiciatis vitae doloremque odio! Error, asperiores unde!',
-        //   selected: false
-        // },
-        // {
-        //   id: 5,
-        //   name: 'Buzz5',
-        //   image: 'https://images.punkapi.com/v2/2.png',
-        //   description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio voluptates tenetur odit fugit, quos laboriosam facilis velit eos commodi omnis pariatur nesciunt ipsa perspiciatis vitae doloremque odio! Error, asperiores unde!',
-        //   selected: false
-        // }
-      ],
-      queryInput: '',
-      searchQuery: ''
+      cards: [],
+      queryInput: ''
     }
 
     this.toggleSelect = this.toggleSelect.bind(this)
@@ -88,7 +51,6 @@ class App extends Component {
   }
 
   execQuery() {
-    console.log(this.state.queryInput)
     fetch('https://api.punkapi.com/v2/beers/' + this.state.queryInput)
       .then(resp => resp.json())
       .then(data => {
